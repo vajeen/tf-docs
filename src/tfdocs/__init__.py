0d71f89061739a3c936dcfb37f47ae57501b42cd
Vajeen Karunathilaka <vajeen@gmail.com>
2024-02-07 20:57:22 +0100

    Update __init__.py

--- src/tfdocs/__init__.py
+++ src/tfdocs/__init__.py
@@ -17,7 +17,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
-"""Main ansible-lint package."""
+
 from __future__ import annotations
 
 from tfdocs.version import __version__
